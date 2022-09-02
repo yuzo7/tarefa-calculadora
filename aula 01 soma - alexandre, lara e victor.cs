baf802f6a5abc0d0ee3b849c015184dbe4fac072
yuzo7 <112702463+yuzo7@users.noreply.github.com>
2022-09-02 09:12:53 -0300

    Add files via upload

---
+++ aula 01 soma - alexandre, lara e victor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_01_soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero01, numero02, resultado;
+
+            Console.WriteLine("Insira o primeiro numero");
+            numero01 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo numero");
+            numero02 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = numero01 / numero02;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
